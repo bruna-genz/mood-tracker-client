af9639b48c0064d48f94aab38fa392a5533b2777
Bruna <brunagenz92@gmail.com>
2020-08-12 15:44:01 -0300

    Feat: create Home, Login, and Signup components
    - add links to Login and Signup to Home
    - create form on Login and Signup

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Home = () => (
+  <div>
+    <Link to="/login">Log In</Link>
+    <br />
+    <Link to="/signup">Sign Up</Link>
+  </div>
+);
+
+export default Home;
