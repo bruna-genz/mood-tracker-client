b2046ef2b9f55edf1ee928c4c171760575ded052
Bruna <brunagenz92@gmail.com>
2020-08-12 18:58:17 -0300

    Feat: refactor App class to be a functional component using Hooks

--- src/App.js
+++ src/App.js
@@ -1,82 +1,69 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Login from './components/registrations/Login';
 import Signup from './components/registrations/Signup';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoggedIn: false,
-      user: {},
+const App = () => {
+  const [isLoggedIn, setLoginStatus] = useState(false);
+  // eslint-disable-next-line no-unused-vars
+  const [user, setUser] = useState({});
+
+  const handleLogin = data => {
+    setLoginStatus(true);
+    setUser(data.user);
+  };
+
+  const handleLogout = () => {
+    setLoginStatus(false);
+    setUser({});
+  };
+
+  useEffect(() => {
+    const loginStatus = () => {
+      axios.get('http://localhost:3001/logged_in',
+        { withCredentials: true }).then(response => {
+        if (response.data.logged_in) {
+          handleLogin(response);
+        } else {
+          handleLogout();
+        }
+      })
+        .catch(error => console.log('api errors:', error));
     };
-    this.handleLogin = this.handleLogin.bind(this);
-    this.handleLogout = this.handleLogout.bind(this);
-  }
+    loginStatus();
+  }, []);
 
-  handleLogin(data) {
-    this.setState({
-      isLoggedIn: true,
-      user: data.user,
-    });
-  }
-
-  handleLogout() {
-    this.setState({
-      isLoggedIn: false,
-      user: {},
-    });
-  }
-
-  loginStatus() {
-    axios.get('http://localhost:3001/logged_in',
-      { withCredentials: true }).then(response => {
-      if (response.data.logged_in) {
-        this.handleLogin(response);
-      } else {
-        this.handleLogout();
-      }
-    })
-      .catch(error => console.log('api errors:', error));
-  }
-
-  componentDidMount() {
-    this.loginStatus();
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <BrowserRouter>
-          <Switch>
-            <Route
-              exact
-              path="/"
-              render={props => (
-                <Home {...props} handleLogout={this.handleLogout} loggedInStatus={this.state.isLoggedIn} />
-              )}
-            />
-            <Route
-              exact
-              path="/login"
-              render={props => (
-                <Login {...props} handleLogin={this.handleLogin} loggedInStatus={this.state.isLoggedIn} />
-              )}
-            />
-            <Route
-              exact
-              path="/signup"
-              render={props => (
-                <Signup {...props} handleLogin={this.handleLogin} loggedInStatus={this.state.isLoggedIn} />
-              )}
-            />
-          </Switch>
-        </BrowserRouter>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={props => (
+              <Home {...props} handleLogout={handleLogout} loggedInStatus={isLoggedIn} />
+            )}
+          />
+          <Route
+            exact
+            path="/login"
+            render={props => (
+              <Login {...props} handleLogin={handleLogin} loggedInStatus={isLoggedIn} />
+            )}
+          />
+          <Route
+            exact
+            path="/signup"
+            render={props => (
+              <Signup {...props} handleLogin={handleLogin} loggedInStatus={isLoggedIn} />
+            )}
+          />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+};
 
 export default App;
