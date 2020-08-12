24f36187fae00a6ba57d56c8966f5b578d6d9865
Bruna <brunagenz92@gmail.com>
2020-08-12 19:47:48 -0300

    Feat: refactor Form class to be a functional component using Hooks

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,133 +1,104 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { SIGNUP_URL, LOGIN_URL } from '../constants';
 
-class Form extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: '',
-      password: '',
-      password_confirmation: '', //! ONLY SIGNUP
-      errors: '',
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.redirect = this.redirect.bind(this);
-    this.handleErrors = this.handleErrors.bind(this);
-  }
+const Form = props => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirmation, setPasswordConfirmation] = useState('');
+  const [errors, setErrors] = useState('');
+  const { type, history, handleLogin } = props;
 
-  componentWillMount() {
-    return this.props.loggedInStatus ? this.redirect() : null;
-  }
+  // componentWillMount() {
+  //   return this.props.loggedInStatus ? this.redirect() : null;
+  // }
 
-  handleChange(event) {
-    const { name, value } = event.target;
-    this.setState({
-      [name]: value,
-    });
-  }
+  const redirect = () => {
+    history.push('/');
+  };
 
-  handleSubmit(event) {
+  const handleSubmit = event => {
     event.preventDefault();
-    const {
-      username, password, password_confirmation,
-    } = this.state;
-
     const user = {
       username,
       password,
-      password_confirmation,
+      passwordConfirmation,
     };
 
-    const URL = this.props.type === 'signup' ? SIGNUP_URL : LOGIN_URL;
+    const URL = type === 'signup' ? SIGNUP_URL : LOGIN_URL;
 
     axios.post(URL, { user }, { withCredentials: true })
       .then(response => {
         if (response.data.status === 'created' || response.data.logged_in) {
-          this.props.handleLogin(response.data);
-          this.redirect();
+          handleLogin(response.data);
+          redirect();
         } else {
-          this.setState({
-            errors: response.data.errors,
-          });
+          setErrors(response.data.errors);
         }
       })
       .catch(error => console.log('api errors:', error));
-  }
+  };
 
-  redirect() {
-    this.props.history.push('/');
-  }
+  const handleErrors = () => (
+    <div>
+      <ul>
+        {errors.map(error => <li key={error}>{error}</li>)}
+      </ul>
+    </div>
+  );
 
-  handleErrors() {
-    return (
-      <div>
-        <ul>
-          {this.state.errors.map(error => <li key={error}>{error}</li>)}
-        </ul>
-      </div>
-    );
-  }
-
-  render() {
-    const {
-      username, password, password_confirmation,
-    } = this.state;
-
-    return (
-      <div>
-        <h1>{ this.props.type === 'signup' ? 'Sign Up' : 'Log In'}</h1>
+  return (
+    <div>
+      <h1>{ type === 'signup' ? 'Sign Up' : 'Log In'}</h1>
+      {' '}
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="username"
+          type="text"
+          name="username"
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+        />
+        <input
+          placeholder="password"
+          type="password"
+          name="password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
         {' '}
-        <form onSubmit={this.handleSubmit}>
-          <input
-            placeholder="username"
-            type="text"
-            name="username"
-            value={username}
-            onChange={this.handleChange}
-          />
-          <input
-            placeholder="password"
-            type="password"
-            name="password"
-            value={password}
-            onChange={this.handleChange}
-          />
-          {' '}
-          { this.props.type === 'signup'
-            && (
-              <input
-                placeholder="password confirmation"
-                type="password"
-                name="password_confirmation"
-                value={password_confirmation}
-                onChange={this.handleChange}
-              />
-            )}
+        { type === 'signup'
+          && (
+            <input
+              placeholder="password confirmation"
+              type="password"
+              name="password_confirmation"
+              value={passwordConfirmation}
+              onChange={e => setPasswordConfirmation(e.target.value)}
+            />
+          )}
 
-          <button placeholder="submit" type="submit">
-            { this.props.type === 'signup' ? 'Sign Up' : 'Log In' }
-          </button>
-          { this.props.type === 'login'
-            && (
-              <div>
-                or
-                {' '}
-                <Link to="/signup">sign up</Link>
-              </div>
-            )}
-        </form>
+        <button placeholder="submit" type="submit">
+          { type === 'signup' ? 'Sign Up' : 'Log In' }
+        </button>
+        { type === 'login'
+          && (
+            <div>
+              or
+              {' '}
+              <Link to="/signup">sign up</Link>
+            </div>
+          )}
+      </form>
 
-        <div>
-          {
-            this.state.errors ? this.handleErrors() : null
-          }
-        </div>
+      <div>
+        {
+          errors ? handleErrors() : null
+        }
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Form;
