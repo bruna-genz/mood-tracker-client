1432d47b4b20e5c03914f82166e59f9e8def2911
Bruna <brunagenz92@gmail.com>
2020-08-14 19:50:38 -0300

    Feat: edit EvaluationForm but it has erros

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -2,10 +2,12 @@ const LOGIN_STATUS_URL = 'http://localhost:3001/logged_in';
 const SIGNUP_URL = 'http://localhost:3001/users';
 const LOGIN_URL = 'http://localhost:3001/login';
 const LOGOUT_URL = 'http://localhost:3001/logout';
+const MOOD_ELEMENTS_URL = 'http://localhost:3001/mood_elements';
 
 export {
   LOGIN_STATUS_URL,
   SIGNUP_URL,
   LOGIN_URL,
   LOGOUT_URL,
+  MOOD_ELEMENTS_URL,
 };
--- src/containers/EvaluationForm.js
+++ src/containers/EvaluationForm.js
@@ -1,35 +1,62 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { MOOD_ELEMENTS_URL } from '../constants/urls';
 
 const EvaluationForm = () => {
-  const options = ['happy', 'sad', 'worried'];
-  const [mood, setMood] = useState('');
-  const [positive, setPositive] = useState('');
-  const [negative, setNegative] = useState('');
-  const [learned, setLearned] = useState('');
+  const options = ['Very happy', 'Happy', 'Normal', 'Sad', 'Very sad'];
+  const [moodElements, setMoodElements] = useState([]);
+  const [moodInputs, setMoodInputs] = useState({});
+
+  useEffect(() => {
+    const getMoodElements = () => {
+      axios.get(MOOD_ELEMENTS_URL, { withCredentials: true })
+        .then(response => {
+          setMoodElements(response.data.mood_elements);
+        });
+    };
+    getMoodElements();
+
+    setMoodInputs(() => {
+      // eslint-disable-next-line no-return-assign
+      moodElements.forEach(element => moodInputs[element] = '');
+    });
+  }, []);
 
   return (
     <form>
-      <div className="mood-select">
-        { options.map(option => (
-          <div className="radio" key={option}>
+      {moodElements.map(element => {
+        if (element.type === 'option') {
+          return (
+            <div className="mood-select">
+              { options.map(option => (
+                <div className="radio" key={option}>
+                  <input
+                    type="radio"
+                    value={moodInputs[element]}
+                    checked={moodInputs[element] === element}
+                    onChange={e => setMoodInputs({ ...moodInputs, element: e.target.value })}
+                  />
+                  {option}
+                </div>
+              ))}
+            </div>
+          );
+        }
+
+        if (element.type === 'text') {
+          return (
             <input
-              type="radio"
-              value={option}
-              checked={mood === option}
-              onChange={e => setMood(e.target.value)}
+              placeholder={element.name}
+              type="text"
+              name={element.ref}
+              value={moodInputs[element]}
+              onChange={e => setMoodInputs({ ...moodInputs, element: e.target.value })}
             />
-            {option}
-          </div>
-        ))}
-      </div>
-      <input
-        placeholder="What went well today?"
-        type="positive"
-        name="positive"
-        value={positive}
-        onChange={e => setPositive(e.target.value)}
-      />
-      <input
+          );
+        }
+      })}
+
+      {/* <input
         placeholder="What went badly today?"
         type="negative"
         name="negative"
@@ -43,6 +70,7 @@ const EvaluationForm = () => {
         value={learned}
         onChange={e => setLearned(e.target.value)}
       />
+      <input type="submit" value="Add" /> */}
     </form>
   );
 };
