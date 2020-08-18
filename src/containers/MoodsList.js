75bda2485ce883e3e877565c27c75b287cf63eb1
Bruna <brunagenz92@gmail.com>
2020-08-18 12:25:44 -0300

    Feat: create function to format created date at MoodList component

--- src/containers/MoodsList.js
+++ src/containers/MoodsList.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
-import lodash from 'lodash';
+import _ from 'lodash';
+import moment from 'moment';
 import { useSelector, useDispatch } from 'react-redux';
 import { EVALUATIONS_URL } from '../constants/urls';
 import { getMoods } from '../actions/index';
@@ -8,13 +9,20 @@ import { getMoods } from '../actions/index';
 const MoodsList = () => {
   const moodsList = useSelector(state => state.moods.moodsList);
   const dispatch = useDispatch();
+  const groupedMoodsList = Object.entries(_.mapValues(_.groupBy(moodsList, 'created_at')));
+
+  const formatDate = json => json.map(el => ({
+    ...el,
+    created_at: moment(el.created_at).format('DD MMM YY'),
+  }));
 
   useEffect(() => {
     const getList = () => {
       axios.get(EVALUATIONS_URL, { withCredentials: true })
         .then(response => {
           if (response.data.evaluations) {
-            dispatch(getMoods(response.data.evaluations));
+            const formatedData = formatDate(response.data.evaluations);
+            dispatch(getMoods(formatedData));
           }
         })
         .catch(error => console.log('api errors:', error));
@@ -22,15 +30,17 @@ const MoodsList = () => {
     getList();
   }, [dispatch]);
 
-  const group = lodash.groupBy(moodsList, 'created_at');
-
-  console.log(moodsList);
-  console.log(group)
-
   return (
-    <div className="MoodsList">
-      moods
-    </div>
+    <ul className="MoodsList">
+      { groupedMoodsList.map(moodArray => (
+        <li key={moodArray[0]}>
+          <h4>{moodArray[0]}</h4>
+          {moodArray[1].map(evaluation => {
+            return <p key={evaluation.id}>{evaluation.evaluation}</p>
+          })}
+        </li>
+      ))}
+    </ul>
   );
 };
 
