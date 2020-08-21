07c01c86f802c122de79fa32aa135ad30eb350c3
Bruna <brunagenz92@gmail.com>
2020-08-21 14:52:45 -0300

    Fix: edit base url

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,9 +1,11 @@
-const LOGIN_STATUS_URL = 'http://localhost:3001/logged_in';
-const SIGNUP_URL = 'http://localhost:3001/users';
-const LOGIN_URL = 'http://localhost:3001/login';
-const LOGOUT_URL = 'http://localhost:3001/logout';
-const MOOD_ELEMENTS_URL = 'http://localhost:3001/mood_elements';
-const EVALUATIONS_URL = 'http://localhost:3001/evaluations';
+const BASE_URL = 'https://mood-tracker-api.herokuapp.com/';
+
+const LOGIN_STATUS_URL = `${BASE_URL}logged_in`;
+const SIGNUP_URL = `${BASE_URL}users`;
+const LOGIN_URL = `${BASE_URL}login`;
+const LOGOUT_URL = `${BASE_URL}logout`;
+const MOOD_ELEMENTS_URL = `${BASE_URL}mood_elements`;
+const EVALUATIONS_URL = `${BASE_URL}evaluations`;
 
 export {
   LOGIN_STATUS_URL,
